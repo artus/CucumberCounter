f9f7b445a5acb9a6a8ddaf039de598280278c588
artusvranken <artusvranken@gmail.com>
2018-04-05 15:49:30 +0200

    Added a travis.yml.

--- src/test/java/be/artusvranken/counter/testrunner/CucumberRunTest.java
+++ src/test/java/be/artusvranken/counter/testrunner/CucumberRunTest.java
@@ -5,13 +5,13 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 /**
- * The CucumberRunner is the class that binds the feature files and the execution of tests together.
+ * The CucumberRunTest is the class that binds the feature files and the execution of tests together.
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"be.artusvranken.counter.domain"}
 )
-public class CucumberRunner {
+public class CucumberRunTest {
 
 }
